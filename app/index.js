c9d760b79ba6c2334f245f2e737c3b5df0d09ffb
jobscale <jobscale@users.noreply.github.com>
2024-04-19 02:09:34 +0900

    mime suported

--- app/index.js
+++ app/index.js
@@ -4,8 +4,6 @@ const fs = require('fs');
 const createHttpError = require('http-errors');
 const { logger } = require('@jobscale/logger');
 
-const silent = () => undefined;
-
 class App {
   useHeader(req, res) {
     const protocol = req.socket.encrypted ? 'https' : 'http';
@@ -21,28 +19,38 @@ class App {
   }
 
   usePublic(req, res) {
+    const headers = new Headers(req.headers);
     const { url } = req;
     const protocol = req.socket.encrypted ? 'https' : 'http';
-    const headers = new Headers(req.headers);
     const host = headers.get('host');
     const { pathname } = new URL(`${protocol}://${host}${url}`);
     const filePath = path.join(process.cwd(), 'docs', pathname);
-    try {
-      const buf = fs.readFileSync(filePath);
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
-      res.end(buf);
-    } catch (e) {
-      silent(e.message);
-    }
+    if (!fs.existsSync(filePath)) return false;
+    const mime = file => {
+      const ext = path.extname(file).toLowerCase();
+      if (['png', 'jpeg', 'webp', 'gif'].includes(ext)) return `image/${ext}`;
+      if (['jpg'].includes(ext)) return 'image/jpeg';
+      if (['json'].includes(ext)) return 'application/json';
+      if (['pdf'].includes(ext)) return 'application/pdf';
+      if (['zip'].includes(ext)) return 'application/zip';
+      if (['xml'].includes(ext)) return 'application/xml';
+      if (['html', 'svg'].includes(ext)) return 'text/html';
+      if (['txt', 'md'].includes(ext)) return 'text/plain';
+      return undefined;
+    };
+    const stream = fs.createReadStream(filePath);
+    res.writeHead(200, { 'Content-Type': mime(filePath) });
+    stream.pipe(res);
+    return true;
   }
 
   useLogging(req, res) {
     const ts = new Date().toISOString();
     const progress = () => {
-      const remoteIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+      const headers = new Headers(req.headers);
+      const remoteIp = req.get('X-Forwarded-For') || req.socket.remoteAddress;
       const { method, url } = req;
       const protocol = req.socket.encrypted ? 'https' : 'http';
-      const headers = new Headers(req.headers);
       const host = headers.get('host');
       logger.info({
         ts,
@@ -63,14 +71,14 @@ class App {
   }
 
   router(req, res) {
+    const headers = new Headers(req.headers);
     const method = req.method.toUpperCase();
     const { url } = req;
     const protocol = req.socket.encrypted ? 'https' : 'http';
-    const headers = new Headers(req.headers);
     const host = headers.get('host');
     const { pathname, searchParams } = new URL(`${protocol}://${host}${url}`);
     const route = `${method} ${pathname}`;
-    silent({ route, searchParams });
+    logger.debug({ route, searchParams });
     if (route === 'GET /') {
       res.writeHead(200, { 'Content-Type': 'text/plain' });
       res.end(Array.from(headers.entries()).map(([key, value]) => `${key}: ${value}`).join('\n'));
@@ -99,7 +107,7 @@ class App {
   start() {
     return (req, res) => {
       this.useHeader(req, res);
-      this.usePublic(req, res);
+      if (this.usePublic(req, res)) return;
       this.useLogging(req, res);
       this.router(req, res);
     };
--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const http = require('http');
 const { logger } = require('@jobscale/logger');
 const { app } = require('./app');
 
+const PORT = process.env.PORT || 3000;
+
 const main = async () => {
   const server = http.createServer(app);
   const options = {
     host: '0.0.0.0',
-    port: process.env.PORT || 3000,
+    port: PORT,
   };
   server.listen(options, () => {
     logger.info(JSON.stringify({
